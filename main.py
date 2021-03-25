be0c0ba81a1872618d452fb896eb49f383c23015
Stan Haakman <stanhaakman@icloud.com>
2021-03-25 22:23:06 +0100

    Buids koppeltabbel klaar voor toevoeging visitors

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ fill_database()
 converter = Converter()
 converter.products(fieldnames=['_id', 'name', 'brand', 'category', 'deeplink', 'properties.doelgroep', 'fast_mover', 'gender', 'herhaalaankopen', 'price.selling_price'], filename='products.csv')
 
-'''
-Create filter and load in the file. then replace the wanted values.
-
-After that save the new data and print te amount of <null> values in the csv file to check if the filtering process worked.
-'''
+# '''
+# Create filter and load in the file. then replace the wanted values.
+#
+# After that save the new data and print te amount of <null> values in the csv file to check if the filtering process worked.
+# '''
 
 filter_products = FilterProducts()
 filter_products.load_dataframe(filename='products.csv')
@@ -42,11 +42,11 @@ print(filter_products.dataframe.isna().sum())
 absolutepath = os.getcwd()
 
 data_sender = DataSender()
-data_sender.copy_products_csv(pathname = absolutepath + "\products.csv")
+data_sender.copy_products_csv(pathname = absolutepath + "/products.csv")
 
 converter.visitors(fieldnames=['recommendations.segment', 'recommendations.latest_visit'], filename='visitors.csv')
 
-data_sender.copy_visitors_csv(pathname= absolutepath + "\visitors.csv")
+data_sender.copy_visitors_csv(pathname= absolutepath + "/visitors.csv")
 
 converter.sessions(fieldnames=['_id','user_agent.identifier', 'session_start', 'session_end'], filename='sessions.csv')
 
@@ -54,7 +54,7 @@ filter_sessions = FilterSessions()
 filter_sessions.load_dataframe(filename='sessions.csv')
 filter_sessions.save_dataframe()
 
-data_sender.copy_sessions_csv(pathname=absolutepath + "\sessions.csv")
+data_sender.copy_sessions_csv(pathname=absolutepath + "/sessions.csv")
 
 # propertieslst = [
 #                 "bundel_sku",
@@ -86,13 +86,19 @@ data_sender.copy_sessions_csv(pathname=absolutepath + "\sessions.csv")
 #    data_sender.copy_products_properties_csv(pathname = absolutepath + "\products_properties_{}.csv".format(lst))
 #    pass
 
-converter.sessions(fieldnames=['buid', '_id', 'user_agent.identifier'], filename='sessions_buids.csv')
-filter_sessions.load_dataframe(filename='sessions_buids.csv')
-filter_sessions.drop_null('buid')
-filter_sessions.drop_duplicates('buid')
-filter_sessions.replace_buids()
-filter_sessions.drop_column(['identifier'])
-filter_sessions.save_dataframe(filename='sessions_buids.csv')
-data_sender.copy_sessions_buids_csv(pathname=absolutepath + "\sessions_buids.csv")
+
+buids_converter = Converter()
+
+buids_converter.sessions(fieldnames=['buid', '_id', 'user_agent.identifier'], filename='sessions_buids.csv')
+
+filter_sessions_buids = FilterSessions()
+filter_sessions_buids.load_dataframe(filename='sessions_buids.csv')
+filter_sessions_buids.drop_column(['identifier'])
+filter_sessions_buids.replace_buids()
+filter_sessions_buids.drop_null('buid')
+filter_sessions_buids.drop_duplicates('buid')
+filter_sessions_buids.save_dataframe(filename='sessions_buids.csv')
+buids_sender = DataSender()
+buids_sender.copy_sessions_buids_csv(pathname=absolutepath + "/sessions_buids.csv")
 
 # for buids need update function
