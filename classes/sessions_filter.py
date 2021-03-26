c31fe21aac2291bed02fe31cc281aa0dd22df764
P Brouwer <pebro15@hotmail.com>
2021-03-26 10:54:13 +0100

    per ongeluk aan de verkeerde functie gewerkt

--- classes/sessions_filter.py
+++ classes/sessions_filter.py
@@ -34,12 +34,12 @@ class FilterSessions:
             self.dataframe[column] = self.dataframe[column].replace(np.nan, replacement, regex=True)
 
     def drop_null(self,columm_name):
-        self.dataframe = self.dataframe.dropna(subset=[columm_name])
+        self.dataframe.dropna(subset=[columm_name], inplace=True)
         print(self.dataframe.isna().sum())
         pass
     
     def drop_duplicates(self,columm_name):
-        self.dataframe.dropna(subset=[columm_name], inplace=True)
+        self.dataframe.drop_duplicates(subset=[column_names], keep='first', inplace=True)
 
     def has_filter(self):
 
