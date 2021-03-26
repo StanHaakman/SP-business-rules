0832da696aa1d8c4175642341b95d5a44e84a366
Stan Haakman <stanhaakman@icloud.com>
2021-03-26 12:46:27 +0100

    fix dictionary with popular products

--- Rules/simple_reccomendation.py
+++ Rules/simple_reccomendation.py
@@ -13,9 +13,6 @@ def getPopularID(df):
 
 
     for i in result:
-
-        print(i, type(i[0]['id']))
-
         if i[0]['id'] not in idDict.keys():
             idDict[i[0]['id']] = 1
         else:
@@ -23,53 +20,4 @@ def getPopularID(df):
 
     sorted_idDict = dict(sorted(idDict.items(), key=operator.itemgetter(1), reverse=True))
 
-    print(idDict)
-
-    return idDict
-
-
-
-
-
-
-    #
-    #
-    # return True
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    # # create query
-    # query = "SELECT products_idproducts FROM orders a " \
-    #         "FULL JOIN sessions c ON a.sessions_idsessions = c.idsessions " \
-    #         "FULL JOIN has_sale g ON c.idsessions = g.sessions_idsessions " \
-    #         "WHERE (g.has_sale = true)"
-    #
-    # # execute query and save results
-    # cur.execute(query)
-    # con.commit()
-    # items = cur.fetchall()
-    # print(items)
-    #
-    # # turn result list of tuples to regular list
-    # newitems = list(itertools.chain(*items))
-    # print(newitems)
-    #
-    # # create dict keeping track of how many times item has been bought
-    # for i in newitems:
-    #     if i not in idDict.keys():
-    #         idDict[i] = 1
-    #     else:
-    #         idDict[i] += 1
-    #
-    # # sorteer de dictionary op hoogste values
-    # sorted_idDict = dict(sorted(idDict.items(), key=operator.itemgetter(1), reverse=True))
-    #
-    # return sorted_idDict
-
+    return sorted_idDict
