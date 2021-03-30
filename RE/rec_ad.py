8e051ec0aa9901efda3ddf454817ad9baca8e48c
Debug-Sam <74404094+Debug-Sam@users.noreply.github.com>
2021-03-30 16:09:48 +0200

    aanpassingen gedaan aan get most frequent category

--- RE/rec_ad.py
+++ RE/rec_ad.py
@@ -1,9 +1,8 @@
 from classes.rec_ad_create_tabel import Create_rec_ad
-from classes.Get_most_frequent_category import Get_freq
+from classes.rec_prev_freq import Get_freq
 create = Create_rec_ad()
 get_freq = Get_freq()
 
 
 create.create_table()
-df = get_freq.get_dataframe()
-print(df)
+df = get_freq.get_dataframe(id=)
