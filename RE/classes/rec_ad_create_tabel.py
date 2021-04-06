8da6ae77348e0823c95912ff1af9a7baa81e135a
Stan Haakman <stanhaakman@icloud.com>
2021-04-06 10:45:46 +0200

    Rebase everything in the github to regain some oversight

--- RE/classes/rec_ad_create_tabel.py
+++ RE/classes/rec_ad_create_tabel.py
@@ -1,4 +1,4 @@
-from _functions.config import config
+from recom_functions.config import config
 import psycopg2
 import pandas as pd
 
@@ -10,7 +10,7 @@ class Create_rec_ad:
 
     def get_data(self):
         con = self.con
-        df = pd.read_sql_query(f"select idproducts, category, sub_category, target from products where folder_actief = 'Enabled';", con)
+        df = pd.read_sql_query(f"select idproducts, category, sub_category, sub_sub_category, target from products where folder_actief = 'Enabled';", con)
         return df
 
     def create_table(self):
@@ -21,13 +21,14 @@ class Create_rec_ad:
         query_drop = f"DROP TABLE IF EXISTS acties CASCADE;"
         cur.execute(query_drop)
 
-        query_create = f"CREATE TABLE acties (idProducts VARCHAR, category VARCHAR, sub_category VARCHAR, target VARCHAR);"
+        query_create = f"CREATE TABLE acties (idProducts VARCHAR, category VARCHAR, sub_category VARCHAR, sub_sub_category VARCHAR, target VARCHAR);"
         cur.execute(query_create)
 
         for i, row in df.iterrows():
-            query_fill = f"INSERT INTO acties (idProducts, category, sub_category, target) VALUES('%s','%s','%s','%s');" % (row['idproducts'], row['category'],row['sub_category'], row['target'])
+            query_fill = f"INSERT INTO acties (idProducts, category, sub_category, sub_sub_category, target) VALUES('%s','%s','%s','%s','%s');" % (row['idproducts'], row['category'],row['sub_category'], row['sub_sub_category'], row['target'])
             cur.execute(query_fill)
 
         con.commit()
         con.close()
         print(f"database acties created")
+
