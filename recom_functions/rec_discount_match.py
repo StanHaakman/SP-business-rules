f9ed2d5a2c3e49ad59a9e3abe03d3d33a83f8c41
Debug-Sam <74404094+Debug-Sam@users.noreply.github.com>
2021-04-07 14:57:04 +0200

    advertenties zijn aangepast en zijn gevalideerdt

---
+++ recom_functions/rec_discount_match.py
@@ -0,0 +1,30 @@
+import psycopg2
+import pandas as pd
+from recom_functions.config import config
+
+
+
+
+
+class DiscountMatch:
+
+    def __init__(self):
+        db = config()
+        self.con = psycopg2.connect(**db)
+        self.cur = self.con.cursor()
+
+    def iddata_to_df(self, id):
+        query = f"select idproducts, sub_sub_category, doelgroep, target from products where idproducts = '{id}' limit 1;"
+        df = pd.read_sql_query(query, self.con)
+        return df
+
+    def match(self, id):
+        df = self.iddata_to_df(id=id)
+
+        data = []
+        for i, row in df.iterrows():
+            query = f"select idproducts from products where sub_sub_category = '{row['sub_sub_category']}' and doelgroep = '{row['doelgroep']}' and target = '{row['target']}' limit 2; "
+            self.cur.execute(query)
+            data.append(self.cur.fetchall())
+
+        return data
